e3f803981e2b61657a564d06248c627e52e64fbc
Joseph Long <jdl@fastmail.fm>
2014-09-26 23:05:52 -0400

    First stab at automatically adding commit hooks
    Files in the git-template-overlay will be added on top of
    the git installation's default template in a new template
    directory in ~/.git-template/. This includes a default
    pre-commit hook.

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -3,11 +3,21 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for dotfile in ".bash_profile" ".gitconfig" ".inputrc" ".profile" ".vim" ".vimrc"; do
     # if it doesn't exist, or exists but is a symlink...
-    if ! [ -e $HOME/$dotfile ] || [ -h $HOME/$dotfile ]; then
-        rm -f $HOME/$dotfile
-        ln -vs $DIR/$dotfile $HOME/$dotfile
+    if ! [ -e "$HOME/$dotfile" ] || [ -h "$HOME/$dotfile" ]; then
+        rm -f "$HOME/$dotfile"
+        ln -vs "$DIR/$dotfile" "$HOME/$dotfile"
     fi
 done
+
+mkdir -p "$HOME/.git-template/"
+for git_template_dir in "/usr/share/git-core/templates" "/usr/local/share/git-core/templates"; do
+    if [ -d "$git_template_dir/" ]; then
+        cp -Rv "$git_template_dir/" "$HOME/.git-template/"
+        cp -Rv "$DIR/git-template-overlay/"* "$HOME/.git-template/"
+    fi
+done
+
+git config --global init.templatedir "$HOME/.git-template/"
 
 echo -n "Fetching and/or updating git submodules..."
 git submodule init && git submodule update && echo " Done!"
